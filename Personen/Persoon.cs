653d7950581da654173870916c478fe7078574e3
StefVercaemer <stefvercaemer@gmail.com>
2017-12-03 00:27:48 +0100

    Omzetting tekstbestand naar list van class-objecten

---
+++ Personen/Persoon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personen
+{
+    public class Persoon
+    {
+        public string Familienaam { get; set; }
+        public string V { get; set; }
+    }
+}
